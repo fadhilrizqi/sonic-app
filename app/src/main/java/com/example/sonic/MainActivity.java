d210683db88e68f3d412bf529d0263fbfbca6478
Fadhil Rizqinanda <fadhilrizqinanda@gmail.com>
2023-05-11 17:56:10 +0700

    Initial Commit

---
+++ app/src/main/java/com/example/sonic/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.sonic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void Login(View view){
+        Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+        startActivity(intent);
+    }
+}
